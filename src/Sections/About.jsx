91ba30c5e6b88ab3ff1190b0d3eec7fb91f9895b
Kyrilous Nasr <kyrilous.nasr74@qmail.cuny.edu>
2025-10-07 22:19:21 -0400

    Tweaked 'About', trying a new card asthetic for About, Experience, Related Coursework, etc...

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -1,9 +1,11 @@
 function About() {
     return (
         <section className="about" id="about">
-            <h2>About Me</h2>
-            <p>
-                Hello! I'm Kyrilous Nasr, currently a computer science student at CUNY Queens College. I enjoy coding and exploring new technologies.
+            <h2 className="title">About Me</h2>
+            <p> 
+                Hi, I’m Kyrilous Nasr, a Computer Science major at CUNY Queens College. I recently interned at Ubiq, a spatial media startup, where I built features in Swift/SwiftUI 
+                and worked with the team to test and refine the product. The experience gave me a real look at startup development and taught me how to adapt quickly, solve problems with a team, 
+                and see how code directly impacts users. I also work as a substitute paraprofessional for the NYC Department of Education. <br></br> Outside of school and work, I enjoy coding side projects, cars, and gaming.
             </p>
         </section>
     )
