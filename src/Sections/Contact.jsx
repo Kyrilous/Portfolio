9ffcc168a5af4c89a070776ba81ca71b05445550
Kyrilous Nasr <kyrilous.nasr74@qmail.cuny.edu>
2025-10-07 20:37:01 -0400

    More CSS magic

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -2,10 +2,17 @@ function Contact() {
   return (
     <section className="contact" id="contact">
         <h2 className="title">Lets Connect</h2>
-        <a className="github" href="https://github.com/Kyrilous" target='_blank'>
-          <img src="src\assets\github.png" alt="GitHub" className="icon" />
-        </a>
-        <a href="https://www.linkedin.com/in/kyrilous-nasr-896962337/" target='_blank'> LinkedIn</a>  
+          <div className="icons">
+            <a className="github" href="https://github.com/Kyrilous" target='_blank'>
+              <img src="src\assets\github.png" alt="GitHub" className="icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/kyrilous-nasr-896962337/" target='_blank'>
+              <img src="src\assets\linkedin.png" alt="LinkedIn" className="icon" />
+            </a>  
+            <a href="mailto:kyrilousnasr@gmailcom" target='_blank'>
+              <img src="src\assets\email.png" alt="Email" className="icon" />
+            </a>
+          </div>
     </section>
 
   )
