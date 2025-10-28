18f217ff12d4bf736aa2142a6861da374394935e
Kyrilous Nasr <kyrilous.nasr74@qmail.cuny.edu>
2025-10-28 17:52:23 -0400

    Fixed spacing issue in about cards. Changed text-align from Justified to Left.

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -4,8 +4,8 @@ function About() {
             <section className="about" id="about">
                 <h2 className="title">About Me</h2>
                 <p> 
-                    Hi, I’m Kyrilous Nasr, a Computer Science major at CUNY Queens College. I recently interned at Ubiq, a spatial media startup, where I built features in Swift/SwiftUI 
-                    and worked with the team to test and refine the product. The experience gave me a real look at startup development and taught me how to adapt quickly, solve problems with a team, 
+                    Hi, I'm Kyrilous Nasr, a Computer Science major at CUNY Queens College. I recently interned at Ubiq, a spatial media startup, where I built features in Swift/SwiftUI
+                    and worked with the team to test and refine the product. The experience gave me a real look at startup development and taught me how to adapt quickly, solve problems with a team,
                     and see how code directly impacts users. I also work as a substitute paraprofessional for the NYC Department of Education. <br></br> Outside of school and work, I enjoy coding side projects, cars, and gaming.
                 </p>
             </section>
@@ -13,9 +13,9 @@ function About() {
              <section className="about" id="experience">
                 <h2 className="title">Experience</h2>
                 <p> 
-                   <b className="experience-title">Ubiq Spatial Media</b><br></br> 
-                    I interned at Ubiq, a spatial media startup, where I built features in Swift/SwiftUI and worked with the team to test and refine the product. 
-                    The role gave me the chance to improve my technical skills, adapt quickly, and see how the things I built directly shaped the user experience.
+                   <b className="experience-title">Ubiq Spatial Media</b><br></br>
+                    I interned at Ubiq, a spatial media startup, where I build featrures in Swift/SwiftUI and worked with the teams to test and refine the final product.
+                    The role gave me a chance to improce my technical skills, adapt quickly, and see how the things I build directly shaped the user experience.
                     <br></br><br></br>
                    <b className="experience-title">NYC Department of Education</b><br></br> 
                     I work as a substitute paraprofessional for the NYC Department of Education. In this role, I assist students with special needs, help implement individualized education plans (IEPs), 
